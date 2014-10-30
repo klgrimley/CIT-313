3da2640c0a2d650deb93d925ae48937f23c51597
Kevin <klgrimley@hotmail.com>
2014-10-30 19:11:00 -0400

    start ex4

---
+++ ex4/application/controllers/logincontroller.php
@@ -0,0 +1,17 @@
+<?php
+
+class LoginController extends Controller{
+	
+    public function defaultTask() {
+
+        $this->userObject = new User();
+        $this->set('task', 'do_login');
+    }
+   
+   public function do_login(){
+	 
+	   
+   }
+	
+	
+}
