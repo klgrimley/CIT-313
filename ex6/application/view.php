6d3eea0919eaa401d4bcacdef223a97eb6b21860
Kevin <klgrimley@hotmail.com>
2014-11-16 16:09:08 -0500

    issue with single post/member
    Different then what's covered in video. New single post/member view not
    reconigizing variable.

---
+++ ex6/application/view.php
@@ -0,0 +1,16 @@
+<?php
+
+class View {
+   function load( $folder, $file_name, $data = null ) 
+   {
+      if( is_array($data) ) {
+         extract($data);
+      }
+      
+      $u = new User();
+      include 'views/'.$folder.'/'.$file_name.'.php';
+   }
+}
+
+
+
---
+++ ex6/views/home/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'application/config.php';
+include('views/elements/header.php');
+?>
+<div class="container">
+    <div class="page-header">
+        <h1> Hello From the View </h1>
+    </div>
+        <?php
+        if (isset($_GET['Message'])) {
+            echo $_GET['Message'];
+        }
+        ?>
+</div>
+<?php include('views/elements/footer.php'); ?>
